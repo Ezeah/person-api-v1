8dd4dff3e6458970c16241d90b47c0ae50298bbc
Ezeah <ezeahnnamdi@gmail.com>
2023-09-15 17:20:06 +0100

    update package.json

--- database/db.js
+++ database/db.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.DATABASE_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true
         });
 
          console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
